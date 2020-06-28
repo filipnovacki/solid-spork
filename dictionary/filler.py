b4de077fed90f6ce4a908c3cedbb36ac5ab81681
racuni@filip.novacki.org <>
2020-06-28 12:38:01 +0200

    Added templating for sections
    Changed data structure of entries so every word and its descriptors are
    known in which section (first letter) are they in.

--- dictionary/filler.py
+++ dictionary/filler.py
@@ -1,31 +1,18 @@
 from jinja2 import Template
 
-# template = '''
-# Ime\t| Prezime
-# ---------------------
-# {% for ime, prezime in imena -%}
-# {{ ime }}\t| {{ prezime}}
-# {% endfor %}
-# '''
-
 template = open('dictionary.tex.jinja').read()
 
-words = [{
-    'name': 'Aardvark',
-    'pronunciation':'ahrd-vahrk',
-    'pos':'Verb',
+dictionary = [{'section':'A', 'words':[{'name':'Aardvark', 'pronunciation':'ahrd-vahrk', 'pos':'Verb',
     'explanation':'A nocturnal badger-sized burrowing mammal of Africa, with\
     long ears, a tubular snout, and a long extensible tongue, feeding on ants\
-    and termites. Also called antbear.'},
-    {'name': 'Bardvak',
-    'pronunciation':'ahrd-vahrk',
-    'pos':'Verb',
+    and termites. Also called antbear.'}]},
+    {'section':'B', 'words':[{'name':'Bardvak', 'pronunciation':'ahrd-vahrk', 'pos':'Verb',
     'explanation':'A nocturnal badger-sized burrowing mammal of Africa, with\
     long ears, a tubular snout, and a long extensible tongue, feeding on ants\
-    and termites. Also called antbear.'}]
+    and termites. Also called antbear.'}]}]
 
 template = Template(template)
-rendered = template.render(entries = words)
+rendered = template.render(entries = dictionary)
 
 print(rendered)
 
