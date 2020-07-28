6a442c564c88876826fc3d1c7eb1fee928615eb1
Filip Novacki <>
2020-07-28 19:43:31 +0200

    Added comments
    Added comments for explaining which function does what for having
    documentaion

--- server/forms.py
+++ server/forms.py
@@ -3,6 +3,9 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class InputDataForm(FlaskForm):
+    '''
+
+    '''
     dict_name = StringField('Dictionary name', validators=[DataRequired()])
     input_text = StringField('Input text', validators=[DataRequired()])
     submit = SubmitField('Add words to dictionray')
