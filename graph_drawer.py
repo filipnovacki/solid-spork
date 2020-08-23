ba07a0178d003cd271661bac92ee7084778f4929
Filip Novacki <fnovacki@pm.me>
2020-08-23 04:39:07 +0200

    Ammend to previous commit

--- graph_drawer.py
+++ graph_drawer.py
@@ -1,6 +1,7 @@
-from dbapi import get_word_count, get_dicts, get_word_names
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
+
+from dbapi import get_word_count, get_word_names
 
 
 def draw_occ_graph(dictionary):
@@ -13,12 +14,22 @@ def draw_occ_graph(dictionary):
         ax1 = fig.add_subplot(111)
         ax1.set_xlabel('Word occurrences')
         ax1.set_ylabel('Word number')
+        ax1.set_title(dictionary + " - graph ")
         ax1.set_yscale('log', basey=2)
 
         plt.tight_layout()
         plt.grid(True)
         plt.plot(df)
-        plt.savefig('static/' + dictionary + 'wc.png')
+
+        import io, base64
+        output = io.BytesIO()
+        plt.savefig(output, format='png')
+        output.seek(0)
+        fig_png = base64.b64encode(output.getvalue())
+
+        plt.close()
+        return fig_png
+
 
 
 def draw_wordlen_graph(dictionary):
@@ -32,8 +43,17 @@ def draw_wordlen_graph(dictionary):
         ax1 = fig.add_subplot(111)
         ax1.set_xlabel('Word length')
         ax1.set_ylabel('Number of occurrences')
+        ax1.set_title(dictionary + " - graph ")
 
         plt.tight_layout()
         plt.grid(True)
         plt.plot(df)
-        plt.savefig('static/' + dictionary + 'wl.png')
+
+        import io, base64
+        output = io.BytesIO()
+        plt.savefig(output, format='png')
+        output.seek(0)
+        fig_png = base64.b64encode(output.getvalue())
+
+        plt.close()
+        return fig_png
